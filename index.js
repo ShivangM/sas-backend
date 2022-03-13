dfb140e2b348efbc497d0919a7a2807edacfa1cf
ShivangM <shivangmishra0824@gmail.com>
2022-03-13 12:57:08 +0530

    Auth

---
+++ index.js
@@ -0,0 +1,18 @@
+const connectToMongo = require("./db")
+const express = require('express')
+var cors = require('cors')
+require('dotenv').config()
+
+connectToMongo();
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+//Available Routes
+app.use('/api/auth', require("./routes/auth"))
+
+app.listen(port, () => {
+  console.log(`SAS backend listening at port:${port}`)
+})
