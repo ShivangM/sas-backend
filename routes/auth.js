9c5377b0fd253f761e14572b7ec0a9976601d72c
ShivangM <shivangmishra0824@gmail.com>
2022-05-07 17:59:57 +0530

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -73,7 +73,7 @@ router.post('/createuser', [
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const {type,email} = req.body;
+  const {type , email} = req.body;
 
   // Check whether the user with this email exists already
   try {
